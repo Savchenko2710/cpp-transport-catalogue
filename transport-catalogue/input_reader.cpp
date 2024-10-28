aa49ff61bf723cc2d410f97a017bfa5161b0ebed
Savchenko2710 <angelinasavchenko@Angelinas-MacBook-Air.local>
2024-10-28 18:43:26 +0300

    Message

--- transport-catalogue/input_reader.cpp
+++ transport-catalogue/input_reader.cpp
@@ -1,109 +1,85 @@
 #include "input_reader.h"
 
-namespace transport {
+namespace transport_catalogue {
 
-void FillCatalogue(std::istream& in, Catalogue& catalogue) {
-    std::vector<std::string> query_bus;
-    std::vector<std::string> query_stop;
-    std::vector<std::string> query_stop_distances;
-    size_t requests_count;
+void PopulateCatalogue(std::istream& input_stream, Catalogue& transport_catalog) {
+    std::vector<std::string> bus_requests;
+    std::vector<std::string> stop_requests;
+    std::vector<std::string> stop_distance_requests;
+    size_t total_requests;
 
-    in >> requests_count;
-    for (size_t i = 0; i < requests_count; ++i) {
-        std::string keyword, line;
-        in >> keyword;
-        std::getline(in, line);
-        if (keyword == "Stop") {
-            query_stop.push_back(line);
+    input_stream >> total_requests;
+    input_stream.ignore();  // Ignore remaining newline
+
+    for (size_t i = 0; i < total_requests; ++i) {
+        std::string command_type, raw_line;
+        input_stream >> command_type;
+        std::getline(input_stream, raw_line);
+
+        if (command_type == "Stop") {
+            stop_requests.emplace_back(raw_line);
+        } else if (command_type == "Bus") {
+            bus_requests.emplace_back(raw_line);
         }
-        if (keyword == "Bus") {
-            query_bus.push_back(line);
-        }
     }
-    query_stop_distances = query_stop;
-    for (auto& stop : query_stop) {
-        auto [stop_name, coordinates] = FillStop(stop);
-        catalogue.AddStop(stop_name, coordinates);
+    
+    stop_distance_requests = stop_requests;
+    
+    for (const auto& stop_data : stop_requests) {
+        auto [stop_name, coords] = ParseStopData(stop_data);
+        transport_catalog.AddStop(stop_name, coords);
     }
-    for (auto& stop : query_stop_distances) {
-        FillStopDistances(stop, catalogue);
+    
+    for (const auto& distance_data : stop_distance_requests) {
+        ParseAndSetDistances(distance_data, transport_catalog);
     }
-    for (auto& bus : query_bus) {
-        auto [bus_number, stops, is_circle] = FillRoute(bus, catalogue);
-        catalogue.AddRoute(bus_number, stops, is_circle);
-        bus = {};
+    
+    for (const auto& bus_data : bus_requests) {
+        auto [route_number, stop_sequence, is_circular] = ParseBusRoute(bus_data, transport_catalog);
+        transport_catalog.AddRoute(route_number, stop_sequence, is_circular);
     }
 }
 
-std::pair<std::string, geo::Coordinates> FillStop(std::string& line) {
-    std::string stop_name = line.substr(1, line.find_first_of(':') - line.find_first_of(' ') - 1);
-    double lat = std::stod(line.substr(line.find_first_of(':') + 2, line.find_first_of(',') - 1));
-    double lng;
-    line.erase(0, line.find_first_of(',') + 2);
-    if (line.find_last_of(',') == line.npos) {
-        lng = std::stod(line.substr(0, line.npos - 1));
-        line.clear();
-    }
-    else {
-        lng = std::stod(line.substr(0, line.find_first_of(',')));
-        line.erase(0, line.find_first_of(',') + 2);
-    }
-    geo::Coordinates stop_coordinates = { lat, lng };
-
-    return { stop_name, stop_coordinates };
+std::pair<std::string, geo::Coordinates> ParseStopData(const std::string& stop_input) {
+    std::string stop_name = stop_input.substr(0, stop_input.find(':'));
+    std::string coord_part = stop_input.substr(stop_input.find(':') + 1);
+    auto coords = geo::ParseCoordinates(coord_part);
+    return {stop_name, coords};
 }
 
-void FillStopDistances(std::string& line, Catalogue& catalogue) {
-    if (!line.empty()) {
-        std::string stop_from_name = FillStop(line).first;
-        const Stop* from = catalogue.FindStop(stop_from_name);
-
-        while (!line.empty()) {
-            int distanse = 0;
-            std::string stop_to_name;
-            distanse = std::stoi(line.substr(0, line.find_first_of("m to ")));
-            line.erase(0, line.find_first_of("m to ") + 5);
-            if (line.find("m to ") == line.npos) {
-                stop_to_name = line.substr(0, line.npos - 1);
-                const Stop* to = catalogue.FindStop(stop_to_name);
-                catalogue.SetDistance(from, to, distanse);
-                if (!catalogue.GetDistance(from, to)) {
-                    catalogue.SetDistance(to, from, distanse);
-                }
-                line.clear();
-            }
-            else {
-                stop_to_name = line.substr(0, line.find_first_of(','));
-                const Stop* to = catalogue.FindStop(stop_to_name);
-                catalogue.SetDistance(from, to, distanse);
-                if (!catalogue.GetDistance(from, to)) {
-                    catalogue.SetDistance(to, from, distanse);
-                }
-                line.erase(0, line.find_first_of(',') + 2);
-            }
-        }
+void ParseAndSetDistances(const std::string& stop_data, Catalogue& catalogue) {
+    std::string stop_name = stop_data.substr(0, stop_data.find(':'));
+    std::string distance_info = stop_data.substr(stop_data.find(':') + 1);
+    
+    std::istringstream dist_stream(distance_info);
+    std::string segment;
+    while (std::getline(dist_stream, segment, ',')) {
+        auto [distance, destination_stop] = SplitDistanceData(segment);
+        catalogue.SetDistance(stop_name, destination_stop, distance);
     }
 }
 
-std::tuple<std::string, std::vector<const Stop*>, bool> FillRoute(std::string& line, transport::Catalogue& catalogue) {
-    std::vector<const Stop*> route_stops;
-    std::string route_number = line.substr(1, line.find_first_of(':') - 1);
-    line.erase(0, line.find_first_of(':') + 2);
-    bool is_circle = false;
+std::pair<int, std::string> SplitDistanceData(const std::string& segment) {
+    int distance = std::stoi(segment.substr(0, segment.find("m to ")));
+    std::string destination_stop = segment.substr(segment.find("m to ") + 5);
+    return {distance, destination_stop};
+}
+
+std::tuple<std::string, std::vector<const Stop*>, bool> ParseBusRoute(const std::string& bus_input, Catalogue& catalogue) {
+    std::string route_id = bus_input.substr(0, bus_input.find(':'));
+    std::string stops_data = bus_input.substr(bus_input.find(':') + 1);
+    bool is_circular = stops_data.find('-') == std::string::npos;
+
+    std::vector<const Stop*> stops;
+    std::istringstream stops_stream(stops_data);
     std::string stop_name;
-    auto pos = line.find('>') != line.npos ? '>' : '-';
-    while (line.find(pos) != line.npos) {
-        stop_name = line.substr(0, line.find_first_of(pos) - 1);
-        const Stop* stop_name_pointer = catalogue.FindStop(stop_name);
-        route_stops.push_back(stop_name_pointer);
-        line.erase(0, line.find_first_of(pos) + 2);
+    char delimiter = is_circular ? '>' : '-';
+
+    while (std::getline(stops_stream, stop_name, delimiter)) {
+        stops.push_back(catalogue.FindStop(stop_name));
     }
-    stop_name = line.substr(0, line.npos - 1);
-    const Stop* stop_name_pointer = catalogue.FindStop(stop_name);
-    route_stops.push_back(stop_name_pointer);
-    if (pos == '>') is_circle = true;
 
-    return { route_number, route_stops, is_circle };
+    return {route_id, stops, is_circular};
 }
 
-}
+}  
--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -1,83 +1,64 @@
-#include "transport_catalogue.h"
+#include "transport_catalogue.h" 
+
+#include <iostream> 
+#include <deque> 
+#include <string> 
+#include <unordered_map> 
+#include <vector> 
+#include <stdexcept> 
+#include <optional> 
+#include <unordered_set> 
+#include <set> 
 
 namespace transport {
 
-void Catalogue::AddStop(std::string_view stop_name, const geo::Coordinates coordinates) {
-    all_stops_.push_back({ std::string(stop_name), coordinates, {} });
-    stopname_to_stop_[all_stops_.back().name] = &all_stops_.back();
-}
+struct Station {
+    std::string station_name;
+    geo::Coordinates location;
+    std::set<std::string> buses_serving;
+};
 
-void Catalogue::AddRoute(std::string_view bus_number, const std::vector<const Stop*> stops, bool is_circle) {
-    all_buses_.push_back({ std::string(bus_number), stops, is_circle });
-    busname_to_bus_[all_buses_.back().number] = &all_buses_.back();
-    for (const auto& route_stop : stops) {
-        for (auto& stop_ : all_stops_) {
-            if (stop_.name == route_stop->name) stop_.buses_by_stop.insert(std::string(bus_number));
+struct Transit {
+    std::string transit_number;
+    std::vector<const Station*> route_stops;
+    bool circular_route;
+};
+
+struct TransitStats {
+    size_t total_stops;
+    size_t distinct_stations;
+    double path_length;
+    double deviation_ratio;
+};
+
+class Database {
+public:
+    struct DistanceHasher {
+        size_t operator()(const std::pair<const Station*, const Station*>& endpoints) const {
+            size_t start_hash = std::hash<const void*>{}(endpoints.first);
+            size_t end_hash = std::hash<const void*>{}(endpoints.second);
+            return start_hash + end_hash * 41;
         }
-    }
-}
+    };
 
-const Bus* Catalogue::FindRoute(std::string_view bus_number) const {
-    return busname_to_bus_.count(bus_number) ? busname_to_bus_.at(bus_number) : nullptr;
-}
+    void RegisterStation(std::string_view name, const geo::Coordinates coords);
+    void RegisterTransit(std::string_view number, const std::vector<const Station*>& stops, bool circular);
+    
+    const Transit* RetrieveTransit(std::string_view number) const;
+    const Station* RetrieveStation(std::string_view name) const;
+    std::optional<TransitStats> ComputeStats(const std::string_view& number) const;
+    
+    const std::set<std::string>& BusesServingStation(std::string_view name) const;
 
-const Stop* Catalogue::FindStop(std::string_view stop_name) const {
-    return stopname_to_stop_.count(stop_name) ? stopname_to_stop_.at(stop_name) : nullptr;
-}
+    void DefineDistance(const Station* from, const Station* to, int distance);
+    int DistanceBetween(const Station* from, const Station* to) const;
 
-std::optional<BusStat> Catalogue::GetBusStat(const std::string_view& bus_number) const {
-    BusStat bus_stat{};
-    const Bus* bus = FindRoute(bus_number);
+private:
+    std::deque<Transit> all_transits_;
+    std::deque<Station> all_stations_;
+    std::unordered_map<std::string_view, const Transit*> transit_lookup_;
+    std::unordered_map<std::string_view, const Station*> station_lookup_;
+    std::unordered_map<std::pair<const Station*, const Station*>, int, DistanceHasher> station_distances_;
+};
 
-    if (!bus) throw std::invalid_argument("bus not found");
-    if (bus->is_circle) bus_stat.stops_count = bus->stops.size();
-    else bus_stat.stops_count = bus->stops.size() * 2 - 1;
-
-    int route_length = 0;
-    double geographic_length = 0.0;
-
-    for (size_t i = 0; i < bus->stops.size() - 1; ++i) {
-        auto from = bus->stops[i];
-        auto to = bus->stops[i + 1];
-        if (bus->is_circle) {
-            route_length += GetDistance(from, to);
-            geographic_length += geo::ComputeDistance(from->coordinates,
-                to->coordinates);
-        }
-        else {
-            route_length += GetDistance(from, to) + GetDistance(to, from);
-            geographic_length += geo::ComputeDistance(from->coordinates,
-                to->coordinates) * 2;
-        }
-    }
-
-    bus_stat.unique_stops_count = UniqueStopsCount(bus_number);
-    bus_stat.route_length = route_length;
-    bus_stat.curvature = route_length / geographic_length;
-
-    return bus_stat;
-}
-
-size_t Catalogue::UniqueStopsCount(std::string_view bus_number) const {
-    std::unordered_set<std::string_view> unique_stops;
-    for (const auto& stop : busname_to_bus_.at(bus_number)->stops) {
-        unique_stops.insert(stop->name);
-    }
-    return unique_stops.size();
-}
-
-const std::set<std::string> Catalogue::GetBusesByStop(std::string_view stop_name) const {
-    return stopname_to_stop_.at(stop_name)->buses_by_stop;
-}
-
-void Catalogue::SetDistance(const Stop* from, const Stop* to, const int distance) {
-    stop_distances_[{from, to}] = distance;
-}
-
-int Catalogue::GetDistance(const Stop* from, const Stop* to) const {
-    if (stop_distances_.count({ from, to })) return stop_distances_.at({ from, to });
-    else if (stop_distances_.count({ to, from })) return stop_distances_.at({ to, from });
-    else return 0;
-}
-
-}
+} 
